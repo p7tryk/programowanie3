431456757f80b1e91dde373d4bb5dd40377e828f
thisconnect <p7tryk@gmail.com>
2020-01-03 10:05:33 +0100

    overloading more

--- zajecia7/vector.h
+++ zajecia7/vector.h
@@ -22,21 +22,29 @@ class vector2D {
   bool operator>(const vector2D& vec);
   vector2D& operator=(const vector2D& vec);
   vector2D operator+(const vector2D& vec);
+  vector2D operator-(const vector2D& vec);
+  vector2D operator*(double val);
+  double operator*(const vector2D& vec);
+  vector2D operator/(double val);
+  vector2D operator-();
 };
 
 
 vector2D::vector2D(double x, double y)
 {
-  m_x =x;
+  m_x=x;
   m_y=y;
+  printf("konstruktor %p\n",this);
 }
 vector2D::~vector2D()
 {
+  printf("destruktor %p\n",this);
 }
 vector2D::vector2D(const vector2D &vec)
 {
   m_x=vec.m_x;
   m_y=vec.m_y;
+  printf("konstruktor kopiujacy %p\n",this);
 }
 void vector2D::print() const
 {
@@ -64,6 +72,7 @@ bool vector2D::operator>(const vector2D& vec)
 }
 vector2D& vector2D::operator=(const vector2D& vec)
 {
+  printf("operator=%p\n",this);
   if(&vec==this)
     return *this;
 
@@ -73,6 +82,36 @@ vector2D& vector2D::operator=(const vector2D& vec)
 }
   vector2D vector2D::operator+(const vector2D& vec)
   {
+    printf("operator+%p\n",this);
     vector2D temp(m_x+vec.m_x,m_y+vec.m_y);
     return temp;
   }
+vector2D vector2D::operator-(const vector2D& vec)
+  {
+    printf("operator-%p\n",this);
+    vector2D temp(m_x-vec.m_x,m_y-vec.m_y);
+    return temp;
+  }
+vector2D vector2D::operator*(double val)
+{
+  printf("mnozenie przez skalar %p\n",this);
+  vector2D temp(m_x*val,m_y*val);
+  return temp;
+}
+double vector2D::operator*(const vector2D& vec)
+{
+  printf("iloczyn skalarny %p\n",this);
+  return vec.m_x*m_x+vec.m_y*m_y;
+}
+vector2D vector2D::operator/(double val)
+{
+  printf("dzielenie przez skalar %p",this);
+  vector2D temp(m_x/val,m_y/val);
+  return temp;
+}
+vector2D vector2D::operator-()
+{
+  printf("operator negacja %p",this);
+  vector2D temp(-m_x,-m_y);
+  return temp;
+}
--- zajecia7/zadanie1.cpp
+++ zajecia7/zadanie1.cpp
@@ -9,9 +9,12 @@ int main()
   vector2D v1(1,3);
   vector2D v2=v1;
   v2=v2+v1;
+  double temp = v1*v2;
+  v2=v2*temp;
   v1.print();
   v2.print();
-
+  
+  
   printf("%.3lf,%.3lf == %.3lf,%.3lf %s\n",v1.getX(),v1.getY(),v2.getX(),v2.getY(),v1==v2 ? "true" : "false");
   printf("%.3lf,%.3lf != %.3lf,%.3lf %s\n",v1.getX(),v1.getY(),v2.getX(),v2.getY(),v1!=v2 ? "true" : "false");
   printf("%.3lf,%.3lf < %.3lf,%.3lf %s\n",v1.getX(),v1.getY(),v2.getX(),v2.getY(),v1<v2 ? "true" : "false");
